2efb90d7fb5263c563782dddd575eb83807438da
ryvianstyron <amina.khalique@gmail.com>
2014-10-26 15:41:51 -0400

    Term3Game
    First commit for 2.5D game for term 3 RCC Group project - Amina

---
+++ Term3Game/Assets/Player/PlayerActions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerActions : MonoBehaviour 
+{
+	public void PickUpMana(PickupItem Mana)
+    {
+        Mana.ApplyPickupToPlayer();
+    }
+    public void PickUpHealth(PickupItem Health)
+    {
+        Health.ApplyPickupToPlayer();
+    }
+}
